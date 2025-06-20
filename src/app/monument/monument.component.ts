920abaf6ed52ef769f3b45cde6feef04b97fef43
David Martinez <dmartinez@ceste.com>
2025-06-20 10:22:58 +0200

    newMonumentComponent

---
+++ src/app/monument/monument.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-monument',
+  imports: [],
+  templateUrl: './monument.component.html',
+  styleUrl: './monument.component.css'
+})
+export class MonumentComponent {
+
+}
