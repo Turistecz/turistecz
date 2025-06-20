2884daf409a61dd45621041141e0f07c1433315e
David Martinez <dmartinez@ceste.com>
2025-06-20 10:23:33 +0200

    newMonumentListComponent

---
+++ src/app/monument-list/monument-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-monument-list',
+  imports: [],
+  templateUrl: './monument-list.component.html',
+  styleUrl: './monument-list.component.css'
+})
+export class MonumentListComponent {
+
+}
