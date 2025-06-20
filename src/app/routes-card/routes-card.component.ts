27b27123213a9b1fb522382dcc43e1297cc56507
Veronica Gili <vgili@ceste.com>
2025-06-20 14:36:29 +0200

    create Component routes card

---
+++ src/app/routes-card/routes-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-routes-card',
+  imports: [],
+  templateUrl: './routes-card.component.html',
+  styleUrl: './routes-card.component.css'
+})
+export class RoutesCardComponent {
+
+}
