d841ecd2606e4e41ba2229ef152747d7fe1cf399
David Martinez <dmartinez@ceste.com>
2025-06-20 15:13:25 +0200

    individual monument item with input to receive data from parent and remove tags from texts

--- src/app/monument/monument.component.ts
+++ src/app/monument/monument.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-monument',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './monument.component.html',
   styleUrl: './monument.component.css'
 })
-export class MonumentComponent {
+export class MonumentComponent implements OnInit {
+
+  @Input() data = {
+    id: -1,
+    title: " ",
+    description: " ",
+    address: " ",
+    horario: " ",
+    phone: " ",
+    price: " ",
+    image: " ",
+    uri: " "
+  };
+
+  removeHTMLTags(text: string): string {
+    if (text !== undefined){
+      return text.replace(/<[^>]*>/g, '').trim();
+    }
+    else{
+      return "";
+    }
+  };
+
+  ngOnInit() {
+    this.data.description = this.removeHTMLTags(this.data.description);
+    this.data.price = this.removeHTMLTags(this.data.price);
+  };
+
+
 
 }
