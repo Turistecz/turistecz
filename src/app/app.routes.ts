6c6610276c7fc9b763a977c4771e609eebdbe52c
David Martinez <dmartinez@ceste.com>
2025-06-23 13:06:33 +0200

    event and event list components created and access to API done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -17,6 +17,12 @@ export const routes: Routes = [
 
 
     },   
+    {
+        path: 'events', 
+        loadComponent: () =>
+                import('./event-list/event-list.component').then(m => m.EventListComponent)
 
+
+    },   
     
 ];
