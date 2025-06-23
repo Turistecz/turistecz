e9fbddc1580b80fd1f895f68c6e470d461889a80
Veronica Gili <vgili@ceste.com>
2025-06-23 09:27:35 +0200

    import fonts baloo and card finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
+import { RoutesCardComponent } from "./routes-card/routes-card.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, RoutesCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/routes-card/routes-card.component.ts
+++ src/app/routes-card/routes-card.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-routes-card',
   imports: [],
