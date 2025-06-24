a966421ea58db9f3aba97b823bf84ea6db7e89b7
David Martinez <dmartinez@ceste.com>
2025-06-24 09:06:18 +0200

    embbeded map in map component with leaflet

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
-  imports: [],
   templateUrl: './map.component.html',
-  styleUrl: './map.component.css'
+  styleUrls: ['./map.component.css']
 })
-export class MapComponent {
+export class MapComponent implements AfterViewInit {
+  private map: any;
 
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = L.map('map').setView([41.6488, -0.8891], 13); // Zaragoza
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap'
+    }).addTo(this.map);
+
+    L.marker([41.6488, -0.8891]).addTo(this.map)
+      .bindPopup('Zaragoza')
+      .openPopup();
+  }
 }
