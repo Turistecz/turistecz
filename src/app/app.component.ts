8a3e8c0a9e839ebf1cd7cda51a4fa6b59ece0cb3
Veronica Gili <vgili@ceste.com>
2025-06-24 13:00:07 +0200

    card router fixed and the button was added and bg image also

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
-import { RoutesCardListComponent } from "./routes-card-list/routes-card-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, RoutesCardListComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
