76e6e8673fa7f7e1f436a255d3bf0080458a4315
Bau Baucells <abaucells@ceste.com>
2025-06-24 10:39:35 +0200

    sitios route changed and sitio id route added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,18 +7,26 @@ export const routes: Routes = [
         path: '', 
         loadComponent: () =>
                 import('./home/home.component').then(m => m.HomeComponent)
+    
 
 
     },   
+        {
+        path: 'sitios/:id', 
+        loadComponent: () =>
+                import('./monument/monument.component').then(m => m.MonumentComponent)
+
+
+    }, 
     {
-        path: 'monuments', 
+        path: 'sitios', 
         loadComponent: () =>
                 import('./monument-list/monument-list.component').then(m => m.MonumentListComponent)
 
 
     },   
     {
-        path: 'events', 
+        path: 'eventos', 
         loadComponent: () =>
                 import('./event-list/event-list.component').then(m => m.EventListComponent)
 
