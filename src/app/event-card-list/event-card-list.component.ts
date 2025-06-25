8febf728fabb0041ba816b19638ec99c92c4cd54
Veronica Gili <vgili@ceste.com>
2025-06-25 08:47:59 +0200

    create event Card father and son

---
+++ src/app/event-card-list/event-card-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-card-list',
+  imports: [],
+  templateUrl: './event-card-list.component.html',
+  styleUrl: './event-card-list.component.css'
+})
+export class EventCardListComponent {
+
+}
