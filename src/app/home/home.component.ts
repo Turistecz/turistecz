80747eac2b9f34ae61727ec914197bd79b58a914
Bau Baucells <abaucells@ceste.com>
2025-06-25 14:09:59 +0200

    Bootstrap Template Card

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { PlaceCardComponent } from '../place-card/place-card.component';
 import { RoutesCardListComponent } from '../routes-card-list/routes-card-list.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [PlaceCardComponent,RoutesCardListComponent],
+  imports: [PlaceCardComponent, RoutesCardListComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
