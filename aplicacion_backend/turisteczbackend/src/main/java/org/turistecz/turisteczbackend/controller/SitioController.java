655b210e11bf341a726a299e7773799972a7667d
Turistecz <turistecz@gmail.com>
2025-06-30 10:46:21 +0200

    Creacion inicial del backend
    Se ha creado el backend inicial, con algunas entidades y servicios REST funcionales. Es necesario que en la BBDD haya un usuario llamado "rooteador", con el password "msTrky2obX1TpYc"

---
+++ aplicacion_backend/turisteczbackend/src/main/java/org/turistecz/turisteczbackend/controller/SitioController.java
@@ -0,0 +1,37 @@
+package org.turistecz.turisteczbackend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.turistecz.turisteczbackend.model.Sitio;
+import org.turistecz.turisteczbackend.service.SitioService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class SitioController {
+
+	@Autowired	   
+    private SitioService sitioService;
+    
+    @GetMapping("/sitios")
+    public List<Sitio> listar() {
+        return sitioService.buscarTodosSitios();
+    }
+    
+    @GetMapping("/sitiosComoEste")
+    public List<Sitio> listarComoNombre(@RequestParam String nombre) {
+        return sitioService.buscarTodosSitiosDeNombreParecido(nombre);
+    }
+    
+    @PostMapping("/sitios/crear")
+    public String crearSitio(@RequestParam String nombre) {
+    	Sitio s = new Sitio();
+    	s.setNombre(nombre);
+        //Esto solo es lo basico, haria falta poner mas cosas 
+        //dentro del objeto Sitio para rellenar el resto de campos
+        Sitio insertado = sitioService.insertarSitio(s);
+        return "Ha sido creado el sitio " + insertado.getNombre() + " con id = " + insertado.getId();
+    }
+    
+}
