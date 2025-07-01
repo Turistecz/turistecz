87eb7850b1cdf538a1c10e6369ae4b4fc24347f1
Luis Quilelli <luisquilelli14@gmail.com>
2025-07-01 12:40:54 +0200

    created component for routePage

---
+++ src/app/page-routes/page-routes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-routes',
+  imports: [],
+  templateUrl: './page-routes.component.html',
+  styleUrl: './page-routes.component.css'
+})
+export class PageRoutesComponent {
+
+}
