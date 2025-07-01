13774d690806d6d6ec8e0aacc5330d6bde9453f8
Veronica Gili <vgili@ceste.com>
2025-07-01 10:52:02 +0200

    create model event-cad and connect with the Api. We have replicated what is in event and eventlist

---
+++ src/app/event-card-list/event-card.model.ts
@@ -0,0 +1,9 @@
+export interface EventItem {
+  title: string;
+  description: string;
+ 
+}
+
+export interface EventResponse {
+  result: EventItem[];
+}
--- src/app/event-card/event-card.component.ts
+++ src/app/event-card/event-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-event-card',
@@ -8,4 +8,36 @@ import { Component } from '@angular/core';
 })
 export class EventCardComponent {
 
+  // access data from parent 
+  @Input() data = {
+    title: "",
+    description: ""
+    
+  };
+
+  // remove html tags <> from text
+  removeHTMLTags(text: string): string {
+    if (text !== undefined){
+      return text.replace(/<[^>]*>/g, '').trim();
+    }
+    else{
+      return "";
+    }
+  };
+
+  // call the function on start
+  ngOnInit() {
+    this.data.description = this.removeHTMLTags(this.data.description);
+    
+  };
+
+
+
+  getDifferentColor(){
+    const colors = ['#f0f8ff','#2a8dfb'];
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+  }
 }
+
+
