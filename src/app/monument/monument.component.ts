11de88ac97b968f26ef630cfe7797749769f566b
dceste <dmartinez@ceste.com>
2025-07-11 10:37:03 +0200

    console logs erased

--- src/app/monument/monument.component.ts
+++ src/app/monument/monument.component.ts
@@ -110,8 +110,5 @@ export class MonumentComponent implements OnInit {
     this.monumentNumber = Number(this.route.snapshot.paramMap.get('id'));
     this.monumentNumber--;
 
-    console.log(this.monuments);
-    console.log(this.monumentsFiltered);
-
   }
 }
