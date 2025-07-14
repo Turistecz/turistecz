05c8bb748d75d8601ce8376de4c1ed93c013eb96
Alvarooo2332 <asancho@ceste.com>
2025-07-14 09:53:55 +0200

    Login backend and form component created

--- aplicacion_backend/turisteczbackend/src/main/java/org/turistecz/turisteczbackend/controller/UsuarioController.java
+++ aplicacion_backend/turisteczbackend/src/main/java/org/turistecz/turisteczbackend/controller/UsuarioController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/login")
+@RequestMapping("/api")
 public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
@@ -22,17 +22,42 @@ public class UsuarioController {
     //Como estamos usando la anotacion "@GetMapping", las peticiones que vengan al endpoint 
     //tendran que ser de tipo GET.
     @CrossOrigin(origins = "http://localhost:4200")
-    @GetMapping("/usuarios")
+    @GetMapping("/login/usuarios")
     public List<Usuario> listar() {
         return usuarioService.buscarTodosUsuarios();
     }
 
-      @GetMapping("/{id}/nombre")
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping("/login/{id}/nombre")
     public ResponseEntity<String> obtenerNombre(@PathVariable String id) {
         String nombre = usuarioService.encontrarNombrePorId(id);
         return nombre != null ? ResponseEntity.ok(nombre)
                               : ResponseEntity.notFound().build();
 
+  }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @PostMapping ("/login/singin")   
+    public ResponseEntity<?> login(@RequestBody Usuario datosLogin) {
+        Usuario usuario = usuarioService.validarCredenciales(
+            datosLogin.getEmail(),
+            datosLogin.getContrasena()
+        );
+
+        if (usuario != null) {
+            // Evitar devolver la contraseña
+            Usuario respuesta = new Usuario();
+            respuesta.setId(usuario.getId());
+            respuesta.setNombre(usuario.getNombre());
+            respuesta.setApellido(usuario.getApellido());
+            respuesta.setEmail(usuario.getEmail());
+            respuesta.setActivo(usuario.getActivo());
+            respuesta.setFecha_creacion(usuario.getFecha_creacion());
+            return ResponseEntity.ok(respuesta);
+        } else {
+            return ResponseEntity.status(401).body("Credenciales inválidas");
+        }
+    }
 }
 
-}
+
