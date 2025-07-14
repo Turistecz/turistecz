61768d91697d5014695d51b4f769dedce4788ae7
Alvarooo2332 <asancho@ceste.com>
2025-07-14 14:45:13 +0200

    Deleted old code

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,28 +1,3 @@
-/*import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
-
-@Component({
-  selector: 'app-login',
-  imports: [CommonModule,FormsModule],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
-})
-export class LoginComponent {
-    email: string = '';
-  password: string = '';
-
-  onLogin() {
-    if (this.email && this.password) {
-      // Aquí va la lógica de autenticación
-      console.log('Email:', this.email);
-      console.log('Password:', this.password);
-    } else {
-      console.log('Formulario inválido');
-    }
-  }
-}*/
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
