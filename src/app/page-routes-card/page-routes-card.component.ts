9c416dd9d86fd3a20cb426431ead2359a323fdb4
Veronica Gili <vgili@ceste.com>
2025-07-16 09:43:12 +0200

    Routes Service injected in pageRoutes and shows all methods

--- src/app/page-routes-card/page-routes-card.component.ts
+++ src/app/page-routes-card/page-routes-card.component.ts
@@ -1,8 +1,9 @@
 import { Component, inject, Input } from '@angular/core';
-import { imagenRoutes, /*routes*/ } from './routes-card.model';
+import { imagenRoutes} from './routes-card.model';
 import { RoutesService } from '../services/routes.service';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
+import { firstValueFrom } from 'rxjs';
 
 
 @Component({
@@ -17,31 +18,69 @@ export class pageRoutesCardComponent {
 
   constructor(private http: HttpClient,private routeService: RoutesService) {}
 
-@Input() routesName: imagenRoutes=
-   {name:'',
-   src:''}
-  /*
-  ruta: routes = 
-    {
-      id:1,
-      nombre:"",
-      descripcion:""
-    }
+  @Input() routesName: imagenRoutes=
+    {name:'',
+    src:''}
+  
+    /* Carga todas las rutas mapeadas (filtradas) por nombre, descripcion y duracion.*/
+    /* Si quieres mostrar mas campos, añadelos en datos: {}*/
+    async loadAllRoutes(): Promise<any> {
+      try {
+        const datos = await firstValueFrom(this.routeService.getAllRoutes());
+        const allRoutes= datos.map((datos: { nombre: string; descripcion:string; duracion:string}) => {
+          return [datos.nombre,datos.descripcion, datos.duracion]
+        })
 
-  routes: routes[] = [];
- 
-  async loadRoutes(): Promise<any> {
+      console.log(allRoutes)
+      } catch (error) {
+          console.error('Error al cargar Rutas:', error);
+      }   
+    }  
+
+  /*Carga la ruta segun el id */
+  /* El id se coloca en el oninit de abajo */
+    async loadRoutebyId(id: string): Promise<any> {
     try {
-      const datos = await firstValueFrom(this.routeService.getAllRoutes());
-      this.routes = datos;
+      const datos = await firstValueFrom(this.routeService.getRouteById(id));
+      console.log('Ruta recibida:', datos);
+      return datos;
     } catch (error) {
-        console.error('Error al cargar Rutas:', error);
-    }   
-  }  
+      console.error('Error al cargar ruta por ID:', error);
+      throw error;
+    }
+  }
+
+  /*Carga la ruta segun el nombre */
+  /* El nombre se coloca en el oninit de abajo */
+  async loadRoutesbyName(name: string): Promise<any> {
+    try {
+      const datos = await firstValueFrom(this.routeService.routesLikeByName(name));
+      console.log('Ruta recibida:', datos);
+      return datos;
+    } catch (error) {
+      console.error('Error al cargar ruta por ID:', error);
+      throw error;
+    }
+  }
+
+  /*Carga la ruta segun el id */
+  /* El id se coloca en el oninit de abajo */
+  async loadRoutesSite(id: string): Promise<any> {
+    try {
+      const datos = await firstValueFrom(this.routeService.getRouteSites(id));
+      console.log('Ruta recibida:', datos);
+      return datos;
+    } catch (error) {
+      console.error('Error al cargar ruta por ID:', error);
+      throw error;
+    }
+  }
 
   async ngOnInit(): Promise<void> {
-    await this.loadRoutes();
-    console.log(this.routes) 
+  // await this.loadAllRoutes(); // Muestra todas las rutas
+  // await this.loadRoutebyId("1"); // Muestra una ruta segun el id
+  // await this.loadRoutesbyName('Romana'); // Muestra una ruta segun el nombre
+    await this.loadRoutesSite("1"); // Muestra todos los sitios de una ruta segun el id
   }
-  */
+  
 }
--- src/app/services/routes.service.ts
+++ src/app/services/routes.service.ts
@@ -2,20 +2,20 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, Observable, throwError } from 'rxjs';
 
-
 @Injectable({
   providedIn: 'root'
 })
 export class RoutesService {
 
   private routesURL = 'http://localhost:8080/api/rutas';
-  private routeIdURL = 'http://localhost:8080/api/rutaPorID';
-  private routesLikeByNameURL = 'http://localhost:8080/api/rutasParecidas';
-  private routeSitesURL = 'http://localhost:8080/api/sitiosRuta'
+  private routeIdURL = 'http://localhost:8080/api/rutaPorID?id=';
+  private routesLikeByNameURL = 'http://localhost:8080/api/rutasParecidas?nombre=';
+  private routeSitesURL = 'http://localhost:8080/api/sitiosRuta?id='
 
   constructor(private http: HttpClient) { }
 
-  /* Mostrar todas las rutas*/ 
+  /* Mostrar todas las rutas*/
+
   getAllRoutes():Observable<any>{
     return this.http.get(this.routesURL)
     .pipe(
@@ -25,7 +25,7 @@ export class RoutesService {
 
   /*Buscar Rutas por Id*/
   getRouteById(id:string):Observable<any>{
-    return this.http.post(this.routeIdURL,{id})
+    return this.http.get(this.routeIdURL+id)
     .pipe(
       catchError(this.handleError)
     );
@@ -33,7 +33,7 @@ export class RoutesService {
 
   /*Buscar Rutas por Nombre parecido*/
   routesLikeByName(name:string):Observable<any>{
-    return this.http.post(this.routesLikeByNameURL, {name})
+    return this.http.get(this.routesLikeByNameURL+name)
     .pipe(
       catchError(this.handleError)
     )
@@ -41,7 +41,7 @@ export class RoutesService {
 
   /*Muetras los sitios de la Ruta ordenados*/
    getRouteSites(id:string):Observable<any>{
-    return this.http.post(this.routeSitesURL , {id})
+    return this.http.get(this.routeSitesURL+id)
     .pipe(
       catchError(this.handleError)
     )
