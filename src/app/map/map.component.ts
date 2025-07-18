3d11c6fe213de6088f39d28a73ea7e7bd75c4a1d
dceste <dmartinez@ceste.com>
2025-07-18 12:15:03 +0200

    incorporated taxis, buses and trams to map function to show and hide their stops locations

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -2,7 +2,7 @@ import { Component, AfterViewInit, Input, input, OnInit, ElementRef, ViewChild} 
 import * as L from 'leaflet';
 import proj4 from 'proj4';
 import { MapService } from '../services/map.service';
-import { BiziItem } from '../models/map.model';
+import { BiziItem, BusStopItem, TaxiStopItem, TramStopItem } from '../models/map.model';
 import { firstValueFrom } from 'rxjs';
 
 
@@ -18,6 +18,9 @@ export class MapComponent implements AfterViewInit, OnInit{
   private map: any;
 
   bizis: BiziItem[] = [];
+  taxiStops: TaxiStopItem[] = [];
+  busStops: BusStopItem[] = [];
+  tramStops: TramStopItem[] = [];
 
   biziIcon = L.icon({
     iconUrl: 'media/bizi-icon.png',
@@ -68,49 +71,84 @@ export class MapComponent implements AfterViewInit, OnInit{
 
  async ngOnInit(): Promise<void> {
   await this.loadBizis();
+  await this.loadTaxiStops();
+  await this.loadTramStops();
+  await this.loadBusStops();
+  //queda la de bus info
   this.createBiziMarkers();
+  this.createTaxiMarkers();
+  this.createBusMarkers();
+  this.createTramMarkers();
 }
 
-  // function to initialize the map, set the location point
-  private initMap(): void {
-    const coords = this.convertCoords(this.data.latitud, this.data.longitud);
-    const latlng: L.LatLngExpression = [coords[1], coords[0]]; // [lat, lon]
+// function to initialize the map, set the location point
+private initMap(): void {
+  const coords = this.convertCoords(this.data.latitud, this.data.longitud);
+  const latlng: L.LatLngExpression = [coords[1], coords[0]]; // [lat, lon]
 
-    this.map = L.map('map').setView(latlng, 16); // Zaragoza
+  this.map = L.map('map').setView(latlng, 16); // Zaragoza
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '© OpenStreetMap'
-    }).addTo(this.map);
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+  }).addTo(this.map);
 
-    L.marker(latlng).addTo(this.map)
-      .bindPopup(this.name)
-      .openPopup();
-  }
+  L.marker(latlng).addTo(this.map)
+    .bindPopup(this.name)
+    .openPopup();
+}
   
 
-  // Definir UTM zona 30N
-  convertCoords(easting: number, northing: number): [number, number] {
-    proj4.defs("EPSG:32630", "+proj=utm +zone=30T +datum=WGS84 +units=m +no_defs");
+// Definir UTM zona 30N
+convertCoords(easting: number, northing: number): [number, number] {
+  proj4.defs("EPSG:32630", "+proj=utm +zone=30T +datum=WGS84 +units=m +no_defs");
 
-    const utmCoords: [number, number] = [easting, northing];
-    const latLon: [number, number] = proj4("EPSG:32630", "WGS84", utmCoords);
+  const utmCoords: [number, number] = [easting, northing];
+  const latLon: [number, number] = proj4("EPSG:32630", "WGS84", utmCoords);
 
-    return latLon; // [longitud, latitud]
-  }
+  return latLon; // [longitud, latitud]
+}
 
-  async loadBizis(): Promise<void> {
-    try {
-      const datos = await firstValueFrom(this.apiMapService.getBizis());
-      this.bizis = datos.result;
-    
-    } catch (error) {
-      console.error('Error al cargar monumentos:', error);
-    }
-  }
-
+async loadBizis(): Promise<void> {
+  try {
+    const datos = await firstValueFrom(this.apiMapService.getBizis());
+    this.bizis = datos.result;
   
-  // cambiar mapa jaw light y lo de los botones de leyenda (poner taxis buses y tranvia)
+  } catch (error) {
+    console.error('Error al cargar las paradas de bici:', error);
+  }
+}
+
+async loadTaxiStops(): Promise<void> {
+  try {
+    const datos = await firstValueFrom(this.apiMapService.getTaxisStops());
+    this.taxiStops = datos.result;
+
+  } catch (error) {
+    console.error('Error al cargar monumentos:', error);
+  }
+}
+
+async loadTramStops(): Promise<void> {
+  try {
+    const datos = await firstValueFrom(this.apiMapService.getTramsStation());
+    this.tramStops = datos.result;
+
+  } catch (error) {
+    console.error('Error al cargar monumentos:', error);
+  }
+}
+
+async loadBusStops(): Promise<void> {
+  try {
+    const datos = await firstValueFrom(this.apiMapService.getBusesStation());
+    this.busStops = datos.result;
+
+  } catch (error) {
+    console.error('Error al cargar monumentos:', error);
+  }
+}
+
 public showHideMarkers(event: Event, group: L.FeatureGroup): void {
   if ((event.target as HTMLInputElement).checked){
     group.addLayer(this.markerVar);
@@ -131,27 +169,51 @@ public showHideMarkers(event: Event, group: L.FeatureGroup): void {
 // });
 
 private createBiziMarkers(): void {
-    this.bizis.forEach((bizi) => {
-    const coords = bizi.geometry.coordinates;
-    const props = bizi;
+this.createMarkers(this.biziIcon, this.biziMarkerGroup, this.bizis);
+};
 
-    if (!coords || !props) return;
+private createTaxiMarkers(): void {
+  this.createMarkers(this.taxiIcon, this.taxiMarkerGroup, this.taxiStops);
+};
 
-    const lat = coords[1];
-    const lon = coords[0];
+private createBusMarkers(): void {
+  this.createMarkers(this.busIcon, this.busMarkerGroup, this.busStops);
+};
 
-    this.markerVar = L.marker([lat, lon],{ icon: this.biziIcon });//.addTo(this.map);
-    this.biziMarkerGroup.addLayer(this.markerVar);
+private createTramMarkers(): void {
+  this.createMarkers(this.tramIcon, this.tramMarkerGroup, this.tramStops);
+};
 
-    this.markerVar.bindPopup(`
-      <strong>${props.title}</strong><br>
-      Estado: ${props.estado}<br>
-      Bicis: ${props.bicisDisponibles}<br>
-      Anclajes: ${props.anclajesDisponibles}<br>
-      Dirección: ${props.address}
-    `);
+private createMarkers(icon: L.Icon, group: L.FeatureGroup, array: any[]): void {
+  array.forEach((elem) => {
+  const coords = elem.geometry.coordinates;
+  const props = elem;
 
-  });
+  if (!coords || !props) return;
+
+  const lat = coords[1];
+  const lon = coords[0];
+
+  this.markerVar = L.marker([lat, lon],{ icon: icon });//.addTo(this.map);
+  group.addLayer(this.markerVar);
+
+
+    //
+    // TAREAS
+    //
+  // mapa ancho 100% y grande, leyenda abajo y horizontal, check a al izq e icono a al derecha
+  // cambiar mapa jaw light y lo de los botones de leyenda (poner taxis buses y tranvia)
+
+  // como cada uno tiene x datos tendra que ser mas especifico, igual con ifs comprobando si el nombre del grupo tiene bizi o asi y poner cada pop up a cada grupo
+  // this.markerVar.bindPopup(`
+  //   <strong>${props.title}</strong><br>
+  //   Estado: ${props.estado}<br>
+  //   Bicis: ${props.bicisDisponibles}<br>
+  //   Anclajes: ${props.anclajesDisponibles}<br>
+  //   Dirección: ${props.address}
+  // `);
+
+});
 }
 
 }
--- src/app/models/map.model.ts
+++ src/app/models/map.model.ts
@@ -12,7 +12,69 @@ export interface BiziItem {
  
 }
 
+export interface BusStopItem {
+  geometry: {
+  coordinates: [number, number] // [lon, lat]
+  },
+  id: string,
+  title: string,
+  estado: string,
+  address: string,
+  bicisDisponibles: number,
+  anclajesDisponibles: number,
+  description: string
+ 
+}
+
+
+export interface BusInfoItem {
+  geometry: {
+  coordinates: [number, number] // [lon, lat]
+  },
+  id: string,
+  title: string,
+  estado: string,
+  address: string,
+  bicisDisponibles: number,
+  anclajesDisponibles: number,
+  description: string
+ 
+}
+
+export interface TaxiStopItem {
+  geometry: {
+  coordinates: [number, number] // [lon, lat]
+  },
+  id: string,
+  title: string,
+  estado: string,
+  address: string,
+  bicisDisponibles: number,
+  anclajesDisponibles: number,
+  description: string
+ 
+}
+
+export interface TramStopItem {
+  geometry: {
+  coordinates: [number, number] // [lon, lat]
+  },
+  id: number,
+  title: string, 
+}
+
 export interface BiziResponse {
   result: BiziItem[];
 }
-
+export interface BusStopResponse {
+  result: BusStopItem[];
+}
+export interface BusInfoResponse {
+  result: BusInfoItem[];
+}
+export interface TaxiStopResponse {
+  result: TaxiStopItem[];
+}
+export interface TramStopResponse {
+  result: TramStopItem[];
+}
