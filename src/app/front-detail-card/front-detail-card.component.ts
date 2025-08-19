4139ab4b9c47f8f43af42c3e9e40ca65de297d68
veritha-yas <vgili@ceste.com>
2025-08-19 12:21:47 +0200

    created frontDteailCard and sitetextdetail component

---
+++ src/app/front-detail-card/front-detail-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-front-detail-card',
+  imports: [],
+  templateUrl: './front-detail-card.component.html',
+  styleUrl: './front-detail-card.component.css'
+})
+export class FrontDetailCardComponent {
+
+}
