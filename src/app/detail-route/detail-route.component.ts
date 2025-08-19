47d132468d080ac3d2f06d899813c3fe427134ba
veritha-yas <vgili@ceste.com>
2025-08-19 12:00:02 +0200

    created detailRoute component

---
+++ src/app/detail-route/detail-route.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-route',
+  imports: [],
+  templateUrl: './detail-route.component.html',
+  styleUrl: './detail-route.component.css'
+})
+export class DetailRouteComponent {
+
+}
