402312339910f0e1049d8e1505d7a32aba396f19
Alvarooo2332 <asancho@ceste.com>
2025-08-21 14:21:10 +0200

    favorites sitios now in mi-perfil

--- aplicacion_backend/turisteczbackend/src/main/java/org/turistecz/turisteczbackend/repository/FavoritosRepository.java
+++ aplicacion_backend/turisteczbackend/src/main/java/org/turistecz/turisteczbackend/repository/FavoritosRepository.java
@@ -7,11 +7,16 @@ import org.turistecz.turisteczbackend.model.Favoritos;
 
 import jakarta.transaction.Transactional;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface FavoritosRepository extends JpaRepository <Favoritos, Integer>{
-     List<Favoritos> findByUsuario_Id(int usuarioId);
-     @Modifying
+    List<Favoritos> findByUsuarioId(Integer usuarioId);
+    List<Favoritos> findByUsuario_Id(int usuarioId);
+
+    @Modifying
     @Transactional
     @Query("DELETE FROM Favoritos f WHERE f.usuario.id = :usuarioId AND f.sitio.id = :sitioId")
     void deleteByUsuarioAndSitio(@Param("usuarioId") int usuarioId, @Param("sitioId") int sitioId);
@@ -19,3 +24,4 @@ public interface FavoritosRepository extends JpaRepository <Favoritos, Integer>{
     @Query(value = "SELECT * FROM favoritos f WHERE f.usuario_id = :idusuario and f.sitios_id = :idsitio", nativeQuery = true)
     Favoritos comprobarFavorito(@Param("idusuario") int idusuario,@Param ("idsitio") int idsitio);
 }
+
