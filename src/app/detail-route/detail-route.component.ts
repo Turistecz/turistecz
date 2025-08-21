3c3bab0f1cb0408c445736c57e43b5c7b08d7404
veritha-yas <vgili@ceste.com>
2025-08-21 13:17:31 +0200

    WIP: Trying 2 show sites information in details-route.

--- src/app/detail-route/detail-route.component.ts
+++ src/app/detail-route/detail-route.component.ts
@@ -1,5 +1,4 @@
 import { Component, inject } from '@angular/core';
-import { RoutesPage } from '../models/routes.model';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { RoutesService } from '../services/routes.service';
@@ -7,11 +6,11 @@ import { firstValueFrom } from 'rxjs';
 import { FrontDetailCardComponent } from "../front-detail-card/front-detail-card.component";
 import { SiteTextDetailComponent } from "../site-text-detail/site-text-detail.component";
 import { CommonModule } from '@angular/common';
-import { routeDetails } from '../models/details-routes';
+import { routeDetails, sitioResponse } from '../models/details-routes';
 
 @Component({
   selector: 'app-detail-route',
-  imports: [FrontDetailCardComponent, SiteTextDetailComponent,CommonModule],
+  imports: [FrontDetailCardComponent, CommonModule, SiteTextDetailComponent],
   templateUrl: './detail-route.component.html',
   styleUrl: './detail-route.component.css'
 })
@@ -20,8 +19,7 @@ export class DetailRouteComponent {
   private route = inject(ActivatedRoute);
   constructor(private http: HttpClient,private routeService: RoutesService) {}
 
-
-  // routesName: RoutesPage[]=[];
+  /* FRONT DETAIL */
   oneRoute: routeDetails = {
     id:0,
     imagen_destacada: '',
@@ -29,33 +27,25 @@ export class DetailRouteComponent {
     descripcion: '',
   } ;
 
-  /*  INYECCION DEL SERVICIO*/
+  /* SITE & TEXT DETAIL */
+  routesSite: sitioResponse [] =[];
 
-  /* Carga todas las rutas mapeadas (filtradas) por nombre, descripcion y duracion.*/
-  /* Si quieres mostrar mas campos, añadelos en datos: {}*/
+  /* IMAGEN SITIO */
+  // oneSite: sitioDetails = {
+  //     id: '',
+  //     nombre: '',
+  //     url: ''   
+  // }
 
-  // async loadAllRoutes(): Promise<any> {
-  //   try {
-  //     const datos = await this.routeService.getAllRoutes().subscribe(
-  //       datos => {
-  //         this.routesName = datos 
-  //         }
-  //       )
-        
-  //     return datos  
-  //     }   
-  //   catch (error) {
-  //       console.error('Error al cargar Rutas:', error);
-  //   }   
-  // } 
-  
+
+  /* FRONT DETAIL */
   /*Carga la ruta segun el id */
   /* El id se coloca en el oninit de abajo */
   async loadRoutebyId(id: number): Promise<any> {
     try {
       const datos = await firstValueFrom(this.routeService.getRouteById(id));
       this.oneRoute = datos; 
-      console.log(this.oneRoute);
+      // console.log(this.oneRoute);
       return datos;
     } catch (error) {
       console.error('Error al cargar ruta por ID:', error);
@@ -63,23 +53,26 @@ export class DetailRouteComponent {
     }
   }
 
-  /*Carga la ruta segun el nombre */
-  /* El nombre se coloca en el oninit de abajo */
-  // async loadRoutesbyName(name: string): Promise<any> {
-  //   try {
-  //     const datos = await firstValueFrom(this.routeService.routesLikeByName(name));
-  //     return datos;
-  //   } catch (error) {
-  //     console.error('Error al cargar ruta por ID:', error);
-  //     throw error;
-  //   }
-  // }
-
-  // /*Carga la ruta segun el id */
+  /* SITE & TEXT DETAIL */
+  // /*Carga el sitio segun el id */
   /* El id se coloca en el oninit de abajo */
+  async loadRoutesSite(id: string): Promise<any> {
+    try {
+      const datos = await firstValueFrom(this.routeService.getRouteSites(id));
+      this.routesSite = datos;
+      console.log('trae ests rutasss',this.routesSite);
+      return datos;
+    } catch (error) {
+      console.error('Error al cargar ruta por ID:', error);
+      throw error;
+    }
+  }
+
   // async loadRoutesSite(id: string): Promise<any> {
   //   try {
   //     const datos = await firstValueFrom(this.routeService.getRouteSites(id));
+  //     this.oneSite = datos;
+  //     console.log('trae ests rutasss',this.oneSite);
   //     return datos;
   //   } catch (error) {
   //     console.error('Error al cargar ruta por ID:', error);
@@ -88,11 +81,10 @@ export class DetailRouteComponent {
   // }
 
   async ngOnInit(): Promise<void> {
-    // await this.loadAllRoutes(); // Muestra todas las rutas
-    await this.loadRoutebyId(1);  // Muestra una ruta segun el id
-    // await this.loadRoutesbyName('Romana'); // Muestra una ruta segun el nombre
-    // await this.loadRoutesSite("1");  // Muestra todos los sitios de una ruta segun el id
+    await this.loadRoutebyId(1);  // Muestra una ruta segun el id //  FRONT DETAIL 
+    await this.loadRoutesSite("1");  // Muestra todos los sitios de una ruta segun el id // SITE & TEXT DETAIL 
   }
+
 
 }
     
