e3636d00568dbda5716988628bbeb41f2a106df3
veritha-yas <vgili@ceste.com>
2025-08-21 13:17:31 +0200

    WIP: Trying 2 show sites information in details-route.

--- src/app/site-text-detail/site-text-detail.component.ts
+++ src/app/site-text-detail/site-text-detail.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
-import { OnePlaceCardComponent } from "../one-place-card/one-place-card.component";
-import { PlaceCardComponent } from "../place-card/place-card.component";
+import { Component, Input, SimpleChanges} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { sitioResponse } from '../models/details-routes';
+import { RouterLink } from '@angular/router';
+
+
+
 
 @Component({
   selector: 'app-site-text-detail',
-  imports: [OnePlaceCardComponent],
+  imports: [CommonModule],
   templateUrl: './site-text-detail.component.html',
   styleUrl: './site-text-detail.component.css'
 })
 export class SiteTextDetailComponent {
+  constructor() {}
+
+@Input() oneSite: sitioResponse = {
+  imagenes: {
+    id: '',
+    url: '',
+    nombre:''    
+    } 
+  };
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['oneSite']) {
+      console.log('oneSite changed:', changes['oneSite'].currentValue);
+      this.oneSite = {
+        imagenes: {
+          id: changes['oneSite'].currentValue.id,
+          url: changes['oneSite'].currentValue.url,
+          nombre:changes['oneSite'].currentValue.nombre    
+        }   
+      };
+    }
+  }
 
 }
