811c62f7e428238ae6157e9cb6b808ac09a991a7
Alvarooo2332 <asancho@ceste.com>
2025-08-22 15:06:06 +0200

    Created Security for changing email and password

--- aplicacion_backend/turisteczbackend/src/main/java/org/turistecz/turisteczbackend/controller/AuthController.java
+++ aplicacion_backend/turisteczbackend/src/main/java/org/turistecz/turisteczbackend/controller/AuthController.java
@@ -3,10 +3,13 @@ package org.turistecz.turisteczbackend.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import org.turistecz.turisteczbackend.dto.UsuarioDto;
+import org.turistecz.turisteczbackend.dto.ChangeEmailDto;
+import org.turistecz.turisteczbackend.dto.ChangePasswordDto;
+import org.turistecz.turisteczbackend.model.Usuario;
 import org.turistecz.turisteczbackend.service.UsuarioService;
 import org.turistecz.turisteczbackend.service.VerificationTokenService;
+import org.turistecz.turisteczbackend.security.JwtUtil;
 
 @RestController
 @RequestMapping("/auth")
@@ -14,6 +17,8 @@ public class AuthController {
 
     @Autowired
     private UsuarioService usuarioService;
+    @Autowired
+    private JwtUtil jwtUtil;
 
     @Autowired
     private VerificationTokenService verificationTokenService;
@@ -38,4 +43,37 @@ public class AuthController {
             return ResponseEntity.badRequest().body("Token inválido o expirado");
         }
     }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @PutMapping("/change-email")
+    public ResponseEntity<?> changeEmail(
+            @RequestHeader("Authorization") String authHeader,
+            @RequestBody ChangeEmailDto dto) {
+
+        String token = authHeader.substring(7);
+        String email = jwtUtil.getEmail(token);
+
+        Usuario usuario = usuarioService.buscarPorEmail(email);
+        usuarioService.cambiarEmail(usuario.getId(), dto.getNuevoEmail());
+
+        return ResponseEntity.ok("Email actualizado correctamente");
+    }
+
+     @CrossOrigin(origins = "http://localhost:4200")
+    @PutMapping("/change-password")
+    public ResponseEntity<?> changePassword(
+            @RequestHeader("Authorization") String authHeader,
+            @RequestBody ChangePasswordDto dto) {
+
+        String token = authHeader.substring(7);
+        String email = jwtUtil.getEmail(token);
+
+        Usuario usuario = usuarioService.buscarPorEmail(email);
+        usuarioService.cambiarContrasena(usuario.getId(), dto.getActualContrasena(), dto.getNuevaContrasena());
+
+        return ResponseEntity.ok("Contraseña actualizada correctamente");
+    }
+
+
+
 }
