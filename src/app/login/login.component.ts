bb578057e56e1b1dc8d392dac9e722797faa84a1
Alvarooo2332 <asancho@ceste.com>
2025-08-26 14:55:58 +0200

    WIP created recuperar-contrasena component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,43 +1,38 @@
 import { Component } from '@angular/core';
-import { AuthService } from '../services/auth.service';
+import { FormsModule, NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule, FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+  styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent {
   email: string = '';
   password: string = '';
+  forgotEmail: string = '';
+  mostrarRecuperacion: boolean = false;
 
-  constructor(
-    private authService: AuthService,
-    private loginService: LoginService, //  Inyectamos LoginService
-    private router: Router
-  ) {}
-
+  constructor(private http: HttpClient, private router: Router) {}
+  
   onLogin() {
-    if (this.email && this.password) {
-      this.authService.login(this.email, this.password).subscribe({
-        next: (usuario) => {
-          console.log('Login exitoso:');
+    this.http.post('/api/auth/signin', { email: this.email, password: this.password }).subscribe({
+      next: (res: any) => {
+        alert("Login exitoso");
+        this.router.navigate(['/dashboard']); 
+      },
+      error: (err) => alert("Usuario o contraseña incorrectos")
+    });
+  }
 
-          //  Guardamos el usuario correctamente usando el servicio
-          this.loginService.setUsuario(usuario);
-
-          //  Redirigimos (por ejemplo, al home)
-          this.router.navigate(['/mi-perfil']);
-        },
-        error: (err) => {
-          console.error('Error en login:', err);
-          alert('Email o contraseña incorrectos');
-        }
-      });
-    }
+  onForgotPassword() {
+    this.http.post('/api/auth/forgot-password', { email: this.forgotEmail }).subscribe({
+      next: () => alert("Revisa tu correo para restablecer la contraseña"),
+      error: () => alert("Error al enviar el correo")
+    });
   }
 }
