99c0bf7043184b020e82dad5263834f3a5a39497
veritha-yas <vgili@ceste.com>
2025-09-11 14:29:06 +0200

    insert map in detailRouteComponent. Filter info sites to send to map

--- src/app/detail-route/detail-route.component.ts
+++ src/app/detail-route/detail-route.component.ts
@@ -1,24 +1,26 @@
-import { Component, inject, Input, SimpleChanges } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { RoutesService } from '../services/routes.service';
 import { firstValueFrom } from 'rxjs';
 import { FrontDetailCardComponent } from "../front-detail-card/front-detail-card.component";
 import { CommonModule } from '@angular/common';
-import { routeDetails, textoDetails } from '../models/details-routes';
+import { mapRoute, routeDetails, textoDetails } from '../models/details-routes';
 import { TextDetailComponent } from "../text-detail/text-detail.component";
-import { imagenRoutes } from '../models/routes-card.model';
+import { MapComponent } from '../map/map.component';
+import { MonumentServiceService } from '../services/monument-service.service';
+
 
 @Component({
   selector: 'app-detail-route',
-  imports: [FrontDetailCardComponent, CommonModule, TextDetailComponent],
+  imports: [FrontDetailCardComponent, CommonModule, TextDetailComponent, MapComponent],
   templateUrl: './detail-route.component.html',
   styleUrl: './detail-route.component.css'
 })
 export class DetailRouteComponent {
 
   private route = inject(ActivatedRoute);
-  constructor(private http: HttpClient,private routeService: RoutesService) {}
+  constructor(private http: HttpClient, private routeService: RoutesService, private monumentService: MonumentServiceService) {}
 
   /* FRONT DETAIL */
   oneRoute: routeDetails = {
@@ -28,9 +30,15 @@ export class DetailRouteComponent {
     descripcion: '',
   } ;
 
-   idRuta!:number;
+  idRuta!:number;
+
   /* SITE & TEXT DETAIL */
   routeText: textoDetails [] =[];
+
+  /* MAP */
+  idsSitiosRuta:any[]=[];
+  todosSitios:any[]=[];
+  mapRoutes:mapRoute[]=[];
 
   /* FRONT DETAIL */
   /*Carga la ruta segun el id */
@@ -40,9 +48,9 @@ export class DetailRouteComponent {
       const datos = await firstValueFrom(this.routeService.getRouteById(id));
       this.oneRoute = datos;
       this.idRuta = datos.id;
-      console.log("oneroute",this.oneRoute);
       return this.oneRoute;
-    } catch (error) {
+    } 
+    catch (error) {
       console.error('Error al cargar ruta por ID:', error);
       throw error;
     }
@@ -55,22 +63,52 @@ export class DetailRouteComponent {
   async loadRoutesSite(id: number): Promise<any> {
     try {
       const datos = await firstValueFrom(this.routeService.getRouteSites(id));
-      this.routeText = datos;   
-      console.log('routetext:',this.routeText);
-    } catch (error) {
+      this.routeText = datos;
+      /* MAP → recuperar id sitios de la ruta */
+      this.idsSitiosRuta = datos.map((item: any) => item.idSitio);
+    } 
+    catch (error) {
       console.error('Error al cargar ruta por ID:', error);
       throw error;
     }
   }
 
+  /* MAP → traer todos los sitos */
+  async loadSite(): Promise<void> {
+    try {
+      const datos = await firstValueFrom(this.monumentService.getMonumentsNames());
+      this.todosSitios = datos; 
+    } 
+    catch (error) {
+      console.error('Error al cargar ruta por ID:', error);
+      throw error;
+    }
+  }
+
+  /* Filtrar sitios de la ruta */
+  async filtrarSitiosRuta(){
+    this.mapRoutes = this.todosSitios
+    .filter(sitio => this.idsSitiosRuta.includes(sitio.id)) 
+    .map(sitio => ({                                      
+      idSitio: sitio.id,
+      nombre: sitio.nombre,
+      longitud: sitio.longitud,
+      latitud: sitio.latitud
+    }));
+    console.log('Sitios de esta ruta:', this.mapRoutes);
+  }
+
   async ngOnInit(): Promise<void> {
     this.idRuta = Number(this.route.snapshot.paramMap.get('id'));
-
     if (this.idRuta) {
       await this.loadRoutebyId(this.idRuta);
       await this.loadRoutesSite(this.idRuta);
     }
+    /* MAP */
+    await this.loadSite();
+    await this.filtrarSitiosRuta();
   }
+
 }
     
 
