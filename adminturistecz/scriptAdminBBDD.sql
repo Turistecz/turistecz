a45e67e3b95152a3c4b63e9ac5e21cc6969d77b0
Juan Luis Pardo Vela <jpardo@ceste.com>
2025-09-11 11:06:49 +0200

    changed to mysql script, from mariadb

--- adminturistecz/scriptAdminBBDD.sql
+++ adminturistecz/scriptAdminBBDD.sql
@@ -1,59 +1,27 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3307
--- Generation Time: Sep 11, 2025 at 10:51 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Script para MySQL: creación de la base de datos y tablas de administración
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- Crear base de datos si no existe
+CREATE DATABASE IF NOT EXISTS `adminturistecz` 
+  CHARACTER SET utf8mb4 
+  COLLATE utf8mb4_general_ci;
 
+USE `adminturistecz`;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `adminturistecz`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admins`
---
-
-CREATE TABLE `admins` (
-  `nombre` varchar(255) NOT NULL,
-  `correo` varchar(255) NOT NULL,
-  `contrasena` varchar(255) NOT NULL
+-- Crear tabla de administradores
+CREATE TABLE IF NOT EXISTS `admins` (
+  `nombre` VARCHAR(255) NOT NULL,
+  `correo` VARCHAR(255) NOT NULL,
+  `contrasena` VARCHAR(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
---
--- Dumping data for table `admins`
---
-
+-- Insertar datos iniciales en admins
 INSERT INTO `admins` (`nombre`, `correo`, `contrasena`) VALUES
 ('Juan', 'jpardo@ceste.com', '$2y$10$xmuwIMvy5o1Wrq6.KjsNj.VmUkuxfusYBgwrncReZboSdxYVz4YGS'),
 ('a', 'a@gmail.com', '$2y$10$Gp2az/pOr8OvDMkQ0GUTjuAto05573f20./viT4kXtUpxM99DyHbm'),
 ('a', 'a1@gmail.com', '$2y$10$enFdp9SYDT7bKFTPGFXBpeQ5miXkPX54aRMHcuQRDSp/t0KyyHde2');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `login`
---
-
-CREATE TABLE `login` (
-  `correo` varchar(255) NOT NULL,
-  `contrasena` varchar(255) NOT NULL
+-- Crear tabla de login
+CREATE TABLE IF NOT EXISTS `login` (
+  `correo` VARCHAR(255) NOT NULL,
+  `contrasena` VARCHAR(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
