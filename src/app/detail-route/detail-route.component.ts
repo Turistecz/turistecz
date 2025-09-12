a85cb8bb4e9fb0439300b6d7577bd53836b4f50a
veritha-yas <vgili@ceste.com>
2025-09-12 13:45:48 +0200

    detailRoutes and map components updated. route sites watch in map

--- src/app/detail-route/detail-route.component.ts
+++ src/app/detail-route/detail-route.component.ts
@@ -95,7 +95,7 @@ export class DetailRouteComponent {
       longitud: sitio.longitud,
       latitud: sitio.latitud
     }));
-    console.log('Sitios de esta ruta:', this.mapRoutes);
+    // console.log('Sitios de esta ruta:', this.mapRoutes);
   }
 
   async ngOnInit(): Promise<void> {
