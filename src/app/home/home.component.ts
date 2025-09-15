7b1097af96003c39a69cebda593bea7017e369bf
CestePruebas <abaucells@ceste.com>
2025-09-15 15:31:29 +0200

    functional weather element in home (we need to choose a place for it)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PlaceCardComponent } from '../place-card/place-card.component';
 import { RoutesCardListComponent } from '../routes-card-list/routes-card-list.component';
 import { CommonModule } from '@angular/common';
 import { EventCardHomeComponent } from '../event-card-home/event-card-home.component';
 import { EventCardComponent } from "../event-card/event-card.component";
+import { WeatherService } from '../services/weather.service';
 
 @Component({
   selector: 'app-home',
@@ -11,6 +12,31 @@ import { EventCardComponent } from "../event-card/event-card.component";
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
+  current: any;
+
+
+  constructor(private weatherService: WeatherService){}
+
+  ngOnInit(): void {
+    this.weatherService.getWeather(41.662246,-1.025696).subscribe(data => {
+         
+          this.current = data.current;
+          // console.log('tiempo actual', this.current)
+          // console.log('llueve?', this.current.precipitation)
+        
+  })};
+
+getWeatherIcon(current: any): string{
+  if (current.rain > 0 || current.precipitation > 0){
+    let icon = 'fa-cloud-rain text-primary'
+    return icon;
+
+  }else{
+    let icon = 'fa-sun text-warning'
+    return icon;
+  }
+  }
 }
+
