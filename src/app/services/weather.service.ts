b83c801c2b84fbdbd49e23687ca90c0df8d4e596
CestePruebas <abaucells@ceste.com>
2025-09-15 15:31:29 +0200

    functional weather element in home (we need to choose a place for it)

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,17 +1,38 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
+  
+  private apiUrl = 'https://api.open-meteo.com/v1/forecast';
 
   constructor(private http: HttpClient) {}
 
-    getCurrentWeather(lat: number, lon: number): Observable<any> {
-    const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true`;
-    return this.http.get<any>(url);
+   getWeather(lat: number, lon: number): Observable<any> {
+    let params = new HttpParams()
+      .set('latitude', lat)
+      .set('longitude', lon)
+      .set('current', ['temperature_2m', 'precipitation', 'rain'].join(','))
+      .set('timezone', 'auto')
+      .set('forecast_days', 1);
+
+    return this.http.get<any>(this.apiUrl, { params }).pipe(
+      map(response => {
+        const current = response.current;
+        const weatherData = {
+          current: {
+            time: new Date(current.time),
+            temperature_2m: current.temperature_2m,
+            precipitation: current.precipitation,
+            rain: current.rain
+          }
+        };
+        return weatherData;
+      })
+    );
   }
 }
