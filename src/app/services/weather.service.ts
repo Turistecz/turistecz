2da90552f0bb55d3036a3126f216fc7ac3597797
CestePruebas <abaucells@ceste.com>
2025-09-15 11:09:30 +0200

    npm install openmeteo done

---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private http: HttpClient) {}
+
+    getCurrentWeather(lat: number, lon: number): Observable<any> {
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true`;
+    return this.http.get<any>(url);
+  }
+}
