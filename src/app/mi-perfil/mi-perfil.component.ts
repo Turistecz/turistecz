c201aa8bb83933c08c96c2b1718f627f497627fa
Alvarooo2332 <asancho@ceste.com>
2025-09-15 10:43:30 +0200

    WIP fixed add and delete favorite function functionality

--- src/app/mi-perfil/mi-perfil.component.ts
+++ src/app/mi-perfil/mi-perfil.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FavoritosService } from '../services/favoritos.service';
-import { LoginService } from '../services/login.service';
+import { FavoritosService, Sitio } from '../services/favoritos.service';
 import { OnePlaceCardComponent } from '../one-place-card/one-place-card.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-mi-perfil',
@@ -16,51 +13,29 @@ import { forkJoin } from 'rxjs';
 })
 export class MiPerfilComponent implements OnInit {
 
-  favoritos: any[] = [];
+  favoritos: Sitio[] = [];
   usuario: any;
 
-  constructor(
-    private loginService: LoginService,
-    private favoritosService: FavoritosService,
-    private http: HttpClient
-  ) {}
+  constructor(private favoritosService: FavoritosService) {}
 
   ngOnInit(): void {
     const usuarioLS = localStorage.getItem('usuario');
-    if (usuarioLS) {
-      this.usuario = JSON.parse(usuarioLS);
-    } else {
-      console.error('No hay usuario logueado');
-      return;
-    }
+    if (!usuarioLS) return;
 
-    this.cargarFavoritosConImagen();
+    this.usuario = JSON.parse(usuarioLS);
+    this.cargarFavoritos();
   }
 
-  async cargarFavoritosConImagen():Promise<void> {
-    // forkJoin combina varias llamadas HTTP en un solo Observable
-    forkJoin({
-      favoritosData: this.favoritosService.getMisFavoritos(this.usuario.id),
-      sitiosConImagenes: this.http.get<any[]>('http://localhost:8080/api/sitios')
-    }).subscribe({
-      next: ({ favoritosData, sitiosConImagenes }) => {
-        this.favoritos = favoritosData.map(sitio => {
-          // Buscamos la imagen que coincida con el sitio
-          const sitioImagen = sitiosConImagenes.find(s => s.id === sitio.id);
-          const urlImagen = sitioImagen?.imagenes?.length
-            ? `${sitioImagen.imagenes[0].url}`
-           : '/public/images/images_sitios/default.jpg';
-          return {
-            id: sitio.id,
-            nombre: sitio.nombre,
-            url: urlImagen,
-            esFavorito: true
-          };
-        });
+  cargarFavoritos(): void {
+    this.favoritosService.getMisFavoritos(this.usuario.id).subscribe({
+      next: favoritos => {
+        this.favoritos = favoritos.map(fav => ({
+          ...fav,
+          esFavorito: true,
+          url: fav.imagenes?.[0]?.url || '/public/images/images_sitios/default.jpg'
+        }));
       },
-      error: (err) => console.error('Error al cargar favoritos o imágenes:', err)
+      error: err => console.error('Error cargando favoritos:', err)
     });
   }
-  
-  
 }
