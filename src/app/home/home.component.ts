0c90fb143fdde026b793aac0c8e3bb790f2b5e4f
CestePruebas <abaucells@ceste.com>
2025-09-16 09:01:35 +0200

    weather component created

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,8 +3,6 @@ import { PlaceCardComponent } from '../place-card/place-card.component';
 import { RoutesCardListComponent } from '../routes-card-list/routes-card-list.component';
 import { CommonModule } from '@angular/common';
 import { EventCardHomeComponent } from '../event-card-home/event-card-home.component';
-import { EventCardComponent } from "../event-card/event-card.component";
-import { WeatherService } from '../services/weather.service';
 
 @Component({
   selector: 'app-home',
@@ -12,31 +10,6 @@ import { WeatherService } from '../services/weather.service';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent{}
 
-  current: any;
-
-
-  constructor(private weatherService: WeatherService){}
-
-  ngOnInit(): void {
-    this.weatherService.getWeather(41.662246,-1.025696).subscribe(data => {
-         
-          this.current = data.current;
-          // console.log('tiempo actual', this.current)
-          // console.log('llueve?', this.current.precipitation)
-        
-  })};
-
-getWeatherIcon(current: any): string{
-  if (current.rain > 0 || current.precipitation > 0){
-    let icon = 'fa-cloud-rain text-primary'
-    return icon;
-
-  }else{
-    let icon = 'fa-sun text-warning'
-    return icon;
-  }
-  }
-}
-
+ 
---
+++ src/app/weather/weather.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../services/weather.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-weather',
+  imports: [CommonModule],
+  templateUrl: './weather.component.html',
+  styleUrl: './weather.component.css'
+})
+
+export class WeatherComponent implements OnInit{
+
+  current: any;
+
+
+  constructor(private weatherService: WeatherService){}
+
+  ngOnInit(): void {
+    this.weatherService.getWeather(41.662246,-1.025696).subscribe(data => {
+         
+          this.current = data.current;
+  })};
+
+getWeatherIcon(current: any): string{
+  if (current.rain > 0 || current.precipitation > 0){
+    let icon = 'fa-cloud-rain text-primary'
+    return icon;
+
+  }else{
+    let icon = 'fa-sun text-warning'
+    return icon;
+  }
+  }
+}
