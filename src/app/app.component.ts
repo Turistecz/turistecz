b4fc1867dee44d44d1a9b65caa125b681d48a648
Luis Quilelli <luisquilelli14@gmail.com>
2025-09-18 11:14:02 +0200

    add breadcrumbs and arrangements in app.routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,26 +4,27 @@ import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 import { filter } from 'rxjs/operators';
 import { CommonModule } from '@angular/common';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent,CommonModule],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent,CommonModule, BreadcrumbsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'turistecz';
   isModalOpen = false;
+  showBreadcrumbs = false; 
 
   //Para que, al entrar en el enlace, el scroll aparezca arriba del todo
    constructor(private router: Router) { 
     this.router.events
       .pipe(filter((e) => e instanceof NavigationEnd))
-      .subscribe(() => {
+      .subscribe((event: any) => {
+         this.showBreadcrumbs = event.urlAfterRedirects !== '/';
+         
         window.scrollTo(0, 0);
       });
-}
-
-
-
+ }
 }
--- src/app/breadcrumbs/breadcrumbs.component.ts
+++ src/app/breadcrumbs/breadcrumbs.component.ts
@@ -1,11 +1,53 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {  ActivatedRoute, NavigationEnd, Router, RouterModule } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
-  imports: [],
+  imports: [CommonModule,RouterModule],
   templateUrl: './breadcrumbs.component.html',
   styleUrl: './breadcrumbs.component.css'
 })
 export class BreadcrumbsComponent {
+breadcrumbs: Array<{ label: string; url: string }> = [];
 
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+
+    let buildBreadcrumbs = (
+      ruta: ActivatedRoute,
+      url: string = '',
+      breadcrumbs: Array<{ label: string; url: string }> = []
+    ): Array<{ label: string; url: string }> => {
+
+      let enlace = ruta.snapshot.url.map(seg => seg.path).join('/');
+
+      if (enlace) {
+        url += `/${enlace}`;
+      }
+
+      let label = ruta.snapshot.data['breadcrumb'];
+      if (!label && enlace) {
+        label = enlace.charAt(0).toUpperCase() + enlace.slice(1);
+      }
+
+      if (label) {
+        breadcrumbs.push({ label, url });
+      }
+
+      if (ruta.firstChild) {
+        return buildBreadcrumbs(ruta.firstChild, url, breadcrumbs);
+      }
+
+      return breadcrumbs;
+    };
+    
+    this.breadcrumbs = buildBreadcrumbs(this.activatedRoute.root);
+
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.breadcrumbs = buildBreadcrumbs(this.activatedRoute.root);
+      });
+  }
 }
