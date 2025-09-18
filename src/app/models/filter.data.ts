033031dbe6a41f5bc4e7b5159d73d12877c316b0
CestePruebas <abaucells@ceste.com>
2025-09-18 13:41:52 +0200

    icons in sites filters deleted

--- src/app/models/filter.data.ts
+++ src/app/models/filter.data.ts
@@ -5,36 +5,32 @@ export const categories: Category[] = [
         type: 'museos',
         name: 'Museos/Exposiciones',
         keywords: ['museo', 'museum', 'lonja'],
-        icon: 'fa-solid fa-landmark'
     },
     {
         type: 'monumentos',
         name: 'Monumentos/Esculturas',
         keywords: ['puerta', 'estatua', 'murallas'],
-        icon: 'fa-solid fa-monument'
     },
     {
         type: 'zonas-verdes',
         name: 'Zonas verdes',
         keywords: ['parque', 'canal'],
-        icon: 'fa-solid fa-tree'
     },
     {
         type: 'arquitectura',
         name: 'Arquitectura',
         keywords: ['basilica', 'iglesia', 'palacio', 'casa', 'catedral', 'puente', 'zuda', 'mercado'],
-            icon: 'fa-solid fa-building'
     },
     {
         type: 'mudejar',
         name: 'Arte mudéjar',
         keywords: ['aljaferia', 'la seo', 'san pablo', 'magdalena'],
-        icon: 'fa-solid fa-torii-gate'
+
     },
     {
         type: 'romano',
         name: 'Arte romano',
         keywords: ['murallas', 'caesaraugusta'],
-        icon: 'fa-solid fa-archway'
+
     }
 ]
--- src/app/models/filter.model.ts
+++ src/app/models/filter.model.ts
@@ -2,6 +2,6 @@ export interface Category {
     type: string;
     name: string;           // Ej: "Museos/Exposiciones"
     keywords: string[];
-    icon?: string;
+    
 }
 
