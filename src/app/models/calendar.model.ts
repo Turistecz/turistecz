f6f83ec0f2ca455c3d412f0b6a308abf31129868
Yin Medina <yindriska01medina@gmail.com>
2025-10-02 11:17:36 +0200

    Calendar components create and Calendar functional

---
+++ src/app/models/calendar.model.ts
@@ -0,0 +1,13 @@
+export interface CalendarTest {
+  title: string;
+  startDate: string;
+  endDate?: string;
+  subEvent?: {
+    title: string;
+    openingHours?: {
+      dayOfWeek: string;
+      startTime: string;
+      endTime?: string;
+    }[];
+  }[];
+  }
