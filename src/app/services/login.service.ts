bc4881e365f426067cc59e367a6808f25990c071
Yin Medina <yindriska01medina@gmail.com>
2025-12-11 08:42:20 +0100

    I've moved the Landbot code from the index to the mi-perfil component so that it only appears in that component. The Landbot is loaded in loginService.

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
+declare var Landbot: any;
+
 @Injectable({
   providedIn: 'root',
 })
 export class LoginService {
   private usuarioSubject = new BehaviorSubject<any | null>(null);
+  private landbotWidget: any = null;
+  private landbotScript: HTMLScriptElement | null = null;
+  private scriptLoaded = false;
 
   constructor() {
     const guardado = localStorage.getItem('usuario');
@@ -22,6 +27,7 @@ export class LoginService {
   logout(): void {
     this.usuarioSubject.next(null);
     localStorage.removeItem('usuario');
+    this.destroyLandbot();
   }
 
   getUsuario(): string {
@@ -32,10 +38,48 @@ export class LoginService {
     return !!this.usuarioSubject.value;
   }
 
-  // Esto permite a otros componentes suscribirse y reaccionar al cambio
   getUsuarioObservable() {
     return this.usuarioSubject.asObservable();
   }
 
   
+ loadScript(): Promise<void> {
+    return new Promise(resolve => {
+      if (this.scriptLoaded) return resolve();
+
+      const script = document.createElement('script');
+      script.src = 'https://cdn.landbot.io/landbot-3/landbot-3.0.0.mjs';
+      script.type = 'module';
+      script.async = true;
+
+      script.onload = () => {
+        this.scriptLoaded = true;
+        resolve();
+      };
+
+      document.body.appendChild(script);
+    });
+  }
+
+  async init(container: HTMLElement): Promise<void> {
+    await this.loadScript();
+
+    if (!this.landbotWidget) {
+      this.landbotWidget = new Landbot.Container({
+        container: container,
+        configUrl: 'https://storage.googleapis.com/landbot.online/v3/H-3105920-Z876DCRA7X45MZUP/index.json',
+        startVisible: true,
+      });
+    } else {
+      this.landbotWidget.show();
+    }
+  }
+
+  destroyLandbot(): void {
+    if (this.landbotWidget?.destroy) {
+      this.landbotWidget.destroy();
+    }
+    this.landbotWidget = null;
+  }
 }
+
