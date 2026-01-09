84d2f56b3a979423556317ca23cd9195d66ed21a
CestePruebas <abaucells@ceste.com>
2026-01-09 13:55:44 +0100

    calendar is coloring properly. Everything is working

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { CalendarTest } from '../models/calendar.model';
 import { EventService } from '../services/event.service';
 import {IgxCalendarComponent } from "igniteui-angular";
-import { expand } from 'rxjs';
-import { accessibility } from '@igniteui/material-icons-extended';
 
 @Component({
   selector: 'app-calendar',
@@ -35,6 +33,9 @@ private cdr: ChangeDetectorRef
   transStart: Date | null = null;
   transEnd: Date | null = null;
 
+  coloredDates : Date [] = [];
+  fechasUnicas:Date[]=[];
+
   onDateSelected(date: Date | Date[]) {
     this.selectedDate = date instanceof Date ? date : date[0];
     this.filterEventsForDate();
@@ -62,7 +63,7 @@ private cdr: ChangeDetectorRef
             })) 
           }))
         }))
-        
+      this.expandEventDates();
     }, 
       error:(error)=>{
          console.error('Error al obtener los eventos.', error);
@@ -71,92 +72,63 @@ private cdr: ChangeDetectorRef
     })
   }
 
-  // MÉTODO MEJORADO: Genera una fecha por cada día del evento (incluyendo rangos)
- /* private buildSpecialDates(): void {
-    const allDates: Date[] = [];
+ 
+ private colorSpecialDates(): void {
+  //para asegurarnos de que se carga a tiempo para pintarse
+    setTimeout(() => {
+      const items = document.querySelectorAll('igx-day-item');
+      
+      items.forEach(item => {
+        const dateAttr = item.getAttribute('ng-reflect-date');
+        
+        if (dateAttr) {
+          const dateFromCalendar = new Date(dateAttr);
+          //Para que no haya incompatibilidades posibles por horas
+          dateFromCalendar.setHours(0, 0, 0, 0);
 
-    // this.ev.forEach(ev => {
-    //   const start = new Date(ev.startDate);
-    //   const end = ev.endDate ? new Date(ev.endDate) : start;
-    const start = this.transStart;
-    const end = this.transEnd;
+          //comparación para pintar los días que coinciden con los días de eventos
+          const existeEvento = this.coloredDates.some(f => {
+            const fechaEvento = new Date(f);
+            fechaEvento.setHours(0, 0, 0, 0);
+            return fechaEvento.getTime() === dateFromCalendar.getTime();
+          });
 
-      //Generar todas las fechas entre start y end (inclusive)
-      // const currentDate = start;
-      // if(currentDate!== null && end !== null){
-      //   while (currentDate <= end) {
-      //     allDates.push(new Date(currentDate));
-      //     currentDate.setDate(currentDate.getDate() + 1);
-      //   }
-      // }
-    //});
-
-    // const fechasEventos = this.ev.map(evento => {
-    //   return evento.startDate;
-    // });
-
-    // console.log("MAPEEEEEEED", this.mappedEvents)
-    const fechasEventos = this.expandEventDates();
-    console.log("fechasEventos");
-    console.log(fechasEventos);
-
-    
-    const fechasUnicas = fechasEventos.filter((fecha, index) =>{
-      return fechasEventos.indexOf(fecha) === index;
-    });
-
-   //convertimos fecha a timestamps (en segundos)
-    const fechasFiltradasTimestamps = fechasUnicas.map(f => new Date(f).getTime());
-
-    const items = document.querySelectorAll('igx-day-item');
-
-    items.forEach(item => {
-        const ts = Number(item.id); // el timestamp del elemento
-        if (fechasFiltradasTimestamps.includes(ts)) {
+          //lo pinta
+          if (existeEvento) {
             item.classList.add('dia-evento');
+          } else {
+            // Importante limpiar para que no se queden pintados días de meses anteriores
+            item.classList.remove('dia-evento');
+          }
         }
-    });
-
-    this.cdr.detectChanges();
-    
-   
-  }*/
+      });
+      // Forzamos a Angular a enterarse de los cambios de clase
+      this.cdr.detectChanges();
+    }, 100); // delay para esperar a que carguen todos los datos
+}
 
   private expandEventDates() {
     let startEvent : Date;
-    let endEvent : Date;
-    //let idEvent:number;
-    let rangeTotal : Date [] = [];
+    let endEvent : Date | undefined;
     
-    let s = 0;
+    this.coloredDates = [];
 
     this.mappedEvents.forEach(evento => {
-      //idEvent = evento.id;
-      //console.log(evento)
       evento.subEvent?.forEach(sE=>{
-        //console.log(sE)
         startEvent = new Date (sE.startDate);
-        endEvent = new Date (sE.endDate?sE.endDate:"");
 
-        let eventRange:any=[];
+  
         let cursor!:Date;
 
-        if(startEvent!==null && startEvent!==undefined){
-          cursor = startEvent;
+        if(startEvent){
+          cursor = new Date(startEvent);
           
-          if(endEvent===null || endEvent===undefined){
-            endEvent = startEvent;
-          } 
-          if(endEvent!==null||endEvent!==undefined){
-            while (cursor <= endEvent) {
-                eventRange.push(cursor); 
-                cursor.setDate(cursor.getDate() + 1);
-            }
-          }
+          this.coloredDates.push(cursor);
         }
-      rangeTotal.push(eventRange);
       })      
     })
+     this.colorSpecialDates();
+    console.log("fechasUn8icas",this.coloredDates)
 }
 
   private filterEventsForDate() {
@@ -165,48 +137,22 @@ private cdr: ChangeDetectorRef
       return;
     }
 
-    //console.log(this.mappedEvents);
 
     this.eventsOfDay = [];
     console.log("estos son los mapped events");
     console.log(this.mappedEvents);
     this.mappedEvents.forEach(mE=>{
       let subEvent = mE.subEvent;
-      // console.log("mapped sub");
-      // console.log(subEvent)
       subEvent?.forEach(sE=> { 
         this.start = sE.startDate;
         this.transStart = new Date(this.start);
         this.end = sE.endDate?sE.endDate:"";
         this.transEnd = new Date (this.end);
         if(this.transStart.getTime() === this.selectedDate?.getTime()){
-          //console.log("coinciden",subEvent)
           this.eventsOfDay.push(mE)
-        } else {
-          //console.log("no")
-          
-        }
+        } 
       });
-      // console.log("traaaans", this.transStart)
-      // console.log("eventos epicos del dia", this.eventsOfDay)
-      
     })
-    this.expandEventDates();
-      //const isSameDay = selected >= this.transStart && selected <= this.transEnd;
-      
-    //   if(this.transStart!==null && this.transEnd!==null){
-    //     const isSameDay = selected >= this.transStart && selected <= this.transEnd;
-    //   }else if(this.transEnd===null){
-    //     const isSameDay =
-    //   }
-      
-    //   const isValidMonth =
-    //     this.start.getFullYear() > selectedYear ||
-    //     (start.getFullYear() === selectedYear && start.getMonth() >= selectedMonth);
-
-    //   return selected.getDate() === start.getDate() && selected.getMonth() === start.getMonth();*/
-    // }:undefined);
-    
   }
   
 
@@ -256,40 +202,19 @@ private cdr: ChangeDetectorRef
   handleArrowClick(): void {
       console.log("¡flecha pulsada!");
       this.onMonthChanged(this.calendar.viewDate);
-      // NOTA: El calendario de IgniteUI ya debería haber cambiado la vista,
-      // pero si necesitas ejecutar tu lógica de carga de eventos, puedes llamar a:
-      // this.onMonthChanged(this.calendar.viewDate);
   }
 
 
   onMonthChanged(newDate: Date) {
     const visibleDates = this.getCalendarVisibleDates(newDate);
-    //const formatted = this.formatDateToDDMMYYYY(newDate);
-    // this.eventService.getEventstest(formatted).subscribe(data => {
-    //   this.ev = data;
-    //   this.buildSpecialDates();
-    //   this.filterEventsForDate();
-    // });
-    //this.buildSpecialDates();
-   // this.expandEventDates();
-    //this.filterEventsForDate();
+    this.colorSpecialDates();
+    //this.expandEventDates();
+    this.filterEventsForDate();
     this.cdr.detectChanges();
   }
-/*
-  ngOnInit(): void {
-    const today = this.formatDateToDDMMYYYY(new Date());
-
-    this.eventService.getEventstest(today).subscribe(data => {
-      this.ev = data;
-      this.buildSpecialDates();
-      this.filterEventsForDate();
-      this.getCalendarVisibleDates(this.calendar.viewDate);
-    });
-  }*/
 
   ngOnInit(){
     this.showEvents();
-    this.getCalendarVisibleDates(this.calendar.viewDate);
-    // this.buildSpecialDates();
+    this.getCalendarVisibleDates(this.calendar.viewDate);    
   }
 }
